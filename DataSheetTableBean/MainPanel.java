ab38aa0b6695b5e1b39b1a7d64892502b1906d8c
biggujo <gen.style22@gmail.com>
2023-04-20 19:12:28 +0300

    Add event listeners support to main panel

--- DataSheetTableBean/MainPanel.java
+++ DataSheetTableBean/MainPanel.java
@@ -3,23 +3,64 @@ package DataSheetTableBean;
 import DataSheetTableBean.DataSheet.Data;
 import DataSheetTableBean.Panels.ButtonPanel;
 import DataSheetTableBean.Panels.TablePanel;
+import DataSheetTableBean.TableModels.TableModel;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class MainPanel extends JPanel {
     private final JPanel panel;
+    private final JTable table;
+    private final TableModel tableModel;
 
     MainPanel() {
         panel = new JPanel(new BorderLayout(1, 1));
         TablePanel dataSheetTablePanel = new TablePanel();
-        for (int i = 0; i < 100; i++) {
-            dataSheetTablePanel.getTableModel().add(new Data("123", 1, 2));
-        }
+        ButtonPanel buttonPanel = new ButtonPanel();
+        
         dataSheetTablePanel.revalidate();
 
-        panel.add(new ButtonPanel().getPanel(), BorderLayout.SOUTH);
+        panel.add(buttonPanel.getPanel(), BorderLayout.SOUTH);
         panel.add(dataSheetTablePanel.getScrollPanel(), BorderLayout.CENTER);
+
+        table = dataSheetTablePanel.getTable();
+        tableModel = dataSheetTablePanel.getTableModel();
+
+        buttonPanel.getAddButton().addActionListener(e -> {
+            addTableRow();
+            tableModel.fireDataSheetChange();
+        });
+
+        buttonPanel.getDelButton().addActionListener(e -> {
+            System.out.println(tableModel.getDataSheet().size());
+            if (tableModel.getRowCount() > 1) {
+                removeTableRow();
+                tableModel.fireDataSheetChange();
+                return;
+            }
+
+            if (tableModel.getRowCount() == 1) {
+                System.out.println(tableModel.getDataSheet().getData(0));
+                tableModel.getDataSheet().getData(0).setIndex("");
+                tableModel.getDataSheet().getData(0).setX(0);
+                tableModel.getDataSheet().getData(0).setY(0);
+                table.revalidate();
+                table.repaint();
+                tableModel.fireDataSheetChange();
+            }
+        });
+    }
+
+    private void addTableRow() {
+        tableModel.setRowCount(tableModel.getRowCount() + 1);
+        tableModel.getDataSheet().add(new Data());
+        table.revalidate();
+    }
+
+    private void removeTableRow() {
+        tableModel.setRowCount(tableModel.getRowCount() - 1);
+        tableModel.getDataSheet().remove(tableModel.getDataSheet().size() - 1);
+        table.revalidate();
     }
 
     public JPanel getPanel() {
